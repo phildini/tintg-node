20b617148d63f60df0cd17adb4fd4b715bda0f07
Philip James <pjj@philipjohnjames.com>
2013-11-29 10:54:40 -0800

    Initial app commit.

---
+++ server.js
@@ -0,0 +1,20 @@
+var io = require('socket.io').listen(8080);
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+
+var sub = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+sub.auth(redisURL.auth.split(":")[1]);
+
+sub.subscribe('games');
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('room', function(room){
+        socket.join(room);
+    })
+    sub.on('message', function(channel, message){
+        console.log(message);
+        io.sockets.in(message).emit('reload', 'hello');
+    });
+});
