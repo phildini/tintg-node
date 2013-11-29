6b915ec652e1a9d52bba2d394a0bd53f0719d15c
Philip James <pjj@philipjohnjames.com>
2013-11-29 11:00:25 -0800

    Change port binding.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(port);
 var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
